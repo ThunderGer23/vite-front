aa557ddd613ded4d843aafd4b1d49a8384094e37
Erickson Espejo <espejo.erickson@gmail.com>
2022-10-06 00:33:58 -0500

    algo

--- src/components/DocumentDetails/DocumentDetails.jsx
+++ src/components/DocumentDetails/DocumentDetails.jsx
@@ -6,7 +6,9 @@ function DocumentDetails() {
         <>
         <Navbar bg="light" variant="light">
           <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">
+                <img className='logo-header' src="/public/img/mq19.png" alt="" />
+            </Navbar.Brand>
             <Nav className="justify-content-end">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#features">Features</Nav.Link>
