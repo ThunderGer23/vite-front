00cef885c51850a323aa21c672d40a8bd7c782a7
Erickson Espejo <espejo.erickson@gmail.com>
2022-10-31 09:14:33 -0500

    lordicon

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,8 +1,41 @@
+import './AboutUs.css'
+import {Card, Col, Container,Nav,Navbar, Row} from 'react-bootstrap';
+import CardHeader from 'react-bootstrap/esm/CardHeader';
 function AboutUs() {
-    return (  
-        <section>
-            <h3>About Us</h3>
-        </section>
+    return (
+        <>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                    </Navbar.Brand>
+                    <Nav className="justify-content-end">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <Container>
+                <Row>
+                    <Col>
+                    <img className='RotarImagen' src='public/img/mq16.png' />
+                    <img className='RotarImagen' src='public/img/mq17.png' />
+                    <img className='RotarImagen' src='public/img/mq18.png' />
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <CardHeader>
+                                
+                            </CardHeader>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 }
 
--- src/components/DocumentList/DocumentList.jsx
+++ src/components/DocumentList/DocumentList.jsx
@@ -1,22 +1,62 @@
 import './DocumentList.css'
-import {Container,Nav,Navbar} from 'react-bootstrap';
+import { Button, Col, Container, Nav, Navbar, Row, Table } from 'react-bootstrap';
 function DocumentList() {
-    return (  
-        <>
-        <Navbar bg="light" variant="light">
-          <Container>
-            <Navbar.Brand href="#home">
-                <img className='logo-header' src="/public/img/mq19.png" alt="" />
-            </Navbar.Brand>
-            <Nav className="justify-content-end">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        </>
-    );
+  return (
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img className='logo-header' src="/public/img/mq19.png" alt="" />
+          </Navbar.Brand>
+          <Nav className="justify-content-end">
+            <Nav.Link href="#home"><img src='./icons/sad.png' width={'30'} /></Nav.Link>
+            <Nav.Link href="#features"><img src='./icons/document.png' width={'30'} /></Nav.Link>
+            <Nav.Link href="#pricing"><img src='./icons/envelope.png' width={'30'} /></Nav.Link>
+            <Nav.Link href="#pricing"><img src='./icons/settings.png' width={'30'} /></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>File</th>
+                  <th>Porcent</th>
+                  <th>Date</th>
+                  <th>Size</th>
+                  <th>Share</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><img src='./icons/document.png' width={'30'} /> Name</td>
+                  <td></td>
+                  <td>10-22-2020</td>
+                  <td>75pag's</td>
+                  <td>
+                    <Button ><img src='./icons/envelope.png' width={'30'}/></Button>{' '}</td>
+                    <Button ><img src='./icons/trash.gif' width={'30'}/></Button>{' '}
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td colSpan={2}>Larry the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default DocumentList;
